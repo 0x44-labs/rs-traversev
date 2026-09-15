225ae7dbc9c0c96030a6a1427bdcf681c6ecd6b7
Jacob Haap <git@j3.cx>
2026-09-15 18:14:41 +0200

    reduce Preimage nonce size to u64, store time as timestamp seconds

--- src/preimage.rs
+++ src/preimage.rs
@@ -1,29 +1,37 @@
 use core::ops::AddAssign;
-use std::time::SystemTime;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub struct Preimage {
-    time: SystemTime,
-    nonce: u128,
+    time: u64,
+    nonce: u64,
     data: Vec<u8>,
 }
 
 impl Preimage {
     pub fn new(data: &[u8]) -> Self {
+        let time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("system time before Unix epoch")
+            .as_secs();
+
         Self {
-            time: SystemTime::now(),
-            nonce: 0u128,
+            time: time,
+            nonce: 0u64,
             data: data.to_vec(),
         }
     }
 }
 
-impl AddAssign<u128> for Preimage {
-    fn add_assign(&mut self, rhs: u128) {
-        if self.nonce <= u128::MAX - rhs {
+impl AddAssign<u64> for Preimage {
+    fn add_assign(&mut self, rhs: u64) {
+        if self.nonce <= u64::MAX - rhs {
             self.nonce += rhs;
         } else {
-            self.nonce = 0u128;
-            self.time = SystemTime::now();
+            self.nonce = 0u64;
+            self.time = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("system time before Unix epoch")
+                .as_secs();
         }
     }
 }
